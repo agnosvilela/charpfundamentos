9ffb2a3c5614b69d9083732a65fcae2c3ef8921f
agnoslima <agnos.nl@gmail.com>
2024-04-15 22:10:58 -0300

    introdução ao c# .net

---
+++ IntroducaoCsharp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* c# é case sencitive - difernecia maiusculas das minúsculas. Veja abaixo um exemplo*/
+var idade = 27;
+var Idade = 28;
+Console.WriteLine(idade)
+
+/* Comentarios em c# podem ser multilonhas como
+ esta aqui usando barra e asteisco*/
+
+// ou pode ser em linha como esta usando duas barras.
+
