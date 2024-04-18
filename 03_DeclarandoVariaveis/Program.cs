1435603d5c60a1385c13d9d9a7b8e8644bf3ad87
agnoslima <agnos.nl@gmail.com>
2024-04-18 16:33:40 -0300

    Declarando variaveis

---
+++ 03_DeclarandoVariaveis/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DeclarandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando vaiáveis.
+            string nome = "Agnos";
+            decimal altura = 1.71m;
+            int idade = 31;
+
+            Console.WriteLine(nome);
+            Console.WriteLine(altura);
+            Console.WriteLine(idade);
+
+        }
+    }
+}
