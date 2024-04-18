280b86956b8c42ad78df896f1c7d129b3edab3ae
agnoslima <agnos.nl@gmail.com>
2024-04-18 14:38:54 -0300

    Entrada e saida de dados C#

---
+++ 02_EntradaSaidaDados/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace EntradaSaidaDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome completo:");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome);
+
+
+            //Console.Write("Entrada e saida de dados");
+
+        }  
+     }
+}
