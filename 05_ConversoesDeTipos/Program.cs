a114d056333d7853fc4413f285d6aa1992eed9ee
agnoslima <agnos.nl@gmail.com>
2024-04-18 19:51:41 -0300

    conversoes de tipos de dados

---
+++ 05_ConversoesDeTipos/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// conversoes de tipos de dados usando c#
+// conversoes implicitas
+int ano = 2020;
+double ano2 = ano;
+
+Console.WriteLine(ano2);
