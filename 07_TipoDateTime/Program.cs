2b9d3d9ea7ea06babff53182a11816556eb22db7
agnoslima <agnos.nl@gmail.com>
2024-04-19 11:55:08 -0300

    Trabalhando com datas e metodos de datas

---
+++ 07_TipoDateTime/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+var hoje = DateTime.Today;
+var agora = DateTime.Now;
+
+// criando um novo date time
+var data1 = new DateTime(2023, 04, 18);
+var data2 = new DateTime(2023, 04, 18, 11, 23, 30);
+// converte o texto em string em data
+var data3 = DateTime.Parse("18/04/2023");
+
+// metodos de manipulação de datas.
+Console.WriteLine(data2.Day);
+Console.WriteLine(data2.Month);
+Console.WriteLine(data2.Hour);
+Console.WriteLine(data2.Minute);
+Console.WriteLine(data2.Second);
+Console.WriteLine(data2.AddDays(5));
+Console.WriteLine(data2.ToLongDateString());
+Console.WriteLine(data2.ToLongTimeString());
