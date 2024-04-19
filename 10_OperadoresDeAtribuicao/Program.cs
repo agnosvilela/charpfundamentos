6d733f59cfcf39f19201d942619c38ce145f8535
agnoslima <agnos.nl@gmail.com>
2024-04-19 16:28:03 -0300

    trabalhando com Operadores de atribuição

---
+++ 10_OperadoresDeAtribuicao/Program.cs
@@ -0,0 +1,16 @@
+﻿
+var a = 5;
+var b = 5;
+var c = 5;
+var d = 5.0m; 
+var e = 5;
+a += 2; // a = a +2 
+b -= 2;
+c *= 2;
+d /= 2;
+e %= 2;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
