fda1b78946f47454467e27206e93d7b4e8bcba6c
agnoslima <agnos.nl@gmail.com>
2024-04-19 17:01:56 -0300

    Trabalhando com incremento e decremento

---
+++ 11_OperadoresUnariosIncrementoDecremento/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+var a = 5;
+var b = 5;
+
+// incremento unario ++ incrementado em 1
+a++; // ao ler é incrementado
+++a;
+
+    // decremento
+     b--;
+--b;
+// os valores sao inseridos apos finalizar a instrução
+Console.WriteLine(a++);
+Console.WriteLine(b--);
+Console.WriteLine(a);
+Console.WriteLine(a);
+// os valores são inseridos ao executar
+Console.WriteLine(++a);
+Console.WriteLine(--b);
+
