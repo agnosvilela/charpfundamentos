b87b9a6203cd39f23cd9687141c6cff3428b90e7
agnoslima <agnos.nl@gmail.com>
2024-04-19 12:17:48 -0300

    trabalhando com arrays em c#

---
+++ 08_TrabalhandoComArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numeros = new int[3];
+numeros[0] = 1;
+numeros[1] = 3;
+numeros[2] = 4;
+Console.WriteLine(numeros[0]);
+Console.WriteLine(numeros[1]);
+Console.WriteLine(numeros[2]);
+Console.WriteLine(numeros[3]);
