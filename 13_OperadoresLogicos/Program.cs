c3d2eef4a0858fc391ffbd2e21eb30522f656e58
agnoslima <agnos.nl@gmail.com>
2024-04-20 22:00:10 -0300

    trabalhando com operadores logicos

---
+++ 13_OperadoresLogicos/Program.cs
@@ -0,0 +1,8 @@
+﻿// operadore logicos
+var ativo = true;
+var a = 5;
+var b = 6;
+Console.WriteLine(! ativo);
+Console.WriteLine(ativo == true &&  a > b);
+Console.WriteLine(ativo == true || a > b);
+
