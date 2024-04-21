c8393be13bfbf3eba54197ebc0380f002e2fdbae
agnoslima <agnos.nl@gmail.com>
2024-04-20 22:49:21 -0300

    Trabalhando com coondicionais If Else

---
+++ 14_EstruturaCondicionalIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿var idade = 18;
+if(idade >= 18)
+{
+    Console.WriteLine(" É maior de idade.");
+    Console.WriteLine("Cadastro autorizado");
+}
+else
+{
+    Console.WriteLine("É menor de idade.");
+}
+Console.WriteLine("Programa finalizado.");
