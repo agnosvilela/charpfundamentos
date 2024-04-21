a9ca96a840a53f1031ecdd6c0bfb91ceafeef6d9
agnoslima <agnos.nl@gmail.com>
2024-04-20 21:47:35 -0300

    trabalhando com operadores de comparação

---
+++ 12_OperadoresDeComparacao/Program.cs
@@ -0,0 +1,11 @@
+﻿// operadores de comparação - sempre retornam um valor true or false
+var a = 2;
+var b = 3;
+
+Console.WriteLine(a == b);
+Console.WriteLine(a != b);
+Console.WriteLine(a > b);
+Console.WriteLine(a < b);
+Console.WriteLine(a <= b);
+Console.WriteLine(a >= b);
+
