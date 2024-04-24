070f2fe21b1ebeb5ec491449f52d93425aeba810
agnoslima <agnos.nl@gmail.com>
2024-04-23 23:22:13 -0300

    trabalhando com operador ternario

---
+++ 15_OperadorTernario/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+var mensagem = "";
+var nota = 7;
+
+if(nota > 7)
+{
+    Console.WriteLine("aprovado");
+
+}
+else
+{
+Console.WriteLine("Reprovado");
+}
+
