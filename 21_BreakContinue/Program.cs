a74f9be07ad593974e33695334d201c645fcc482
agnoslima <agnos.nl@gmail.com>
2024-04-26 17:32:57 -0300

    Trabalhando com break e continnue

---
+++ 21_BreakContinue/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+var notas = new int[] { 6, 8, 5, 7, 9 };
+var soma = 0.0;
+
+for (int i = 0; i < notas.Length; i++)
+{
+    if (notas[i] < 6)
+    {
+        Console.WriteLine("Nenhuma nota pode ser inferior a 6.");
+        soma = 0;
+        continue;
+    }
+
+    soma += notas[i];
+}
+
+var media = soma / notas.Length;
+
+Console.WriteLine("Média: " + media);
+Console.WriteLine("Hello, World!");
