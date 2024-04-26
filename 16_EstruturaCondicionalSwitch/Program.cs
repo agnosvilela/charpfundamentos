9fca426134cba7af1459b778e2b8ff03ec24ef29
agnoslima <agnos.nl@gmail.com>
2024-04-25 21:11:59 -0300

    trabalhando com estrutura de repetição switch

---
+++ 16_EstruturaCondicionalSwitch/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// trabalhando com a estrutura condicional Switch
+var opcao = 1;
+switch(opcao)
+{
+    case 1:
+        Console.WwriteLine("1");
+        break;
+    case 2:
+        Console.WwriteLine("2");
+        break;
+    case 3:
+        Console.WwriteLine("3");
+        break;
+    default: 
+        {
+            Console.WwriteLine("3");
+            break;
+        }
+}
+Console.WriteLine("programa Finalizado");
