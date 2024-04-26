fc6647149f317f603f2d29e4e316bc750697249b
agnoslima <agnos.nl@gmail.com>
2024-04-26 13:45:28 -0300

    Trabalhando com estrutura de repetição do While

---
+++ 18_EstruturaDeRepeticaoDoWhile/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Estrutura de repetição DO While.
+/*O bloco de codigo é executado pelo menos uma vez e em seguida é verificado a condição.*/
+
+var contador = 10;
+
+while (contador < 10)
+{
+    Console.WriteLine(contador);
+    contador++;
+}
+do
+{
+    Console.WriteLine(contador);
+    contador++;
+}
+while (contador < 10);
+
