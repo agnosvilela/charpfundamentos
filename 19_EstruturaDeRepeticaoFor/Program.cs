26e8e3846fa9bdf7935d63c13d6cb52cafa88f64
agnoslima <agnos.nl@gmail.com>
2024-04-26 16:30:44 -0300

    Trabalhando com estrutura de repetição for

---
+++ 19_EstruturaDeRepeticaoFor/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// laço de repetição for.
+string[] nomes = new string[] {"agnos", "Suelen" };
+
+for(var contador = 0; contador < nomes.Length; contador++)
+{
+    Console.WriteLine(nomes[contador]);
+}
+
+for(var indice = 0; indice < 5; indice++)
+{
+    Console.WriteLine(indice);
+}
+
